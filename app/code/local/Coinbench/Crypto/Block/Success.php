8dd395133d78062539af717eda7c708c28ff8d35
happymeerkat <james.mikkelson@gmail.com>
2014-06-22 15:37:32 +0100

    Checkout success/instructions placeholder

---
+++ app/code/local/Coinbench/Crypto/Block/Success.php
@@ -0,0 +1,13 @@
+<?php
+class Coinbench_Crypto_Block_Success extends Mage_Core_Block_Template
+{
+	protected function _construct()
+	{
+		parent::_construct();
+	}
+
+	public function methodBlock()
+	{
+		return 'address and qr code placeholder' ;
+	}
+}
